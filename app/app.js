c8dabdcc6acf9fc7dd00a9e58e453b8dd4b6b3f6
Simo <a>
2017-06-14 20:26:27 +0300

    init

--- app/app.js
+++ app/app.js
@@ -11,13 +11,22 @@ config(['$locationProvider', '$routeProvider', function($locationProvider, $rout
   $locationProvider.hashPrefix('!');
 
   $routeProvider
-      .when('/stav',
+      .when('/',
           {
-            templateUrl: 'View.Home/View.Home.html'
+              templateUrl: 'Templates/Main/Home.html'
+              // controller: 'homeController'
+
           })
       .when('/simo',
           {
               templateUrl: 'view2/view2.html'
           })
       .otherwise({redirectTo: 'index.html'});
-}]);
+}
+    .controller('homeController',function ($scope,$http) {
+        $http.get("http://localhost:8888/movies/bestfive").then(function (response) {
+            $scope.movies = JSON.parse(response.data);
+            console.log("1")
+        })
+    })
+]);
