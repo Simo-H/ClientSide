3a08d52c8342c87a339516449c8f32df44943ba2
Simo <a>
2017-06-18 13:20:39 +0300

    init

--- app/app.js
+++ app/app.js
@@ -121,7 +121,7 @@ var app = angular.module('myApp', [
             $scope.moviesNew = response;
         });
     })
-    .controller('moviesController', function ($scope, $http, $log, $uibModal,ShoppingDetails) {
+    .controller('moviesController', function ($scope, $http, $log, $uibModal,ShoppingDetails,MoviesUtilities) {
         $scope.categories = new Array('action', 'animation', 'sci-fi', 'comics');
         $scope.searchByCategory = "";
         $scope.searchByMovieName = "";
@@ -140,7 +140,7 @@ var app = angular.module('myApp', [
             $http.get("http://localhost:8888/movies/getNextMovies?limit=6&category=" + category + "&rownum=" + from).success(function (response) {
                 $scope.movisByCategory[category].push.apply($scope.movisByCategory[category], response);
             });
-        }
+        };
         $scope.viewMovie = function (movie) {
 
             var modalInstance = $uibModal.open({
@@ -160,6 +160,13 @@ var app = angular.module('myApp', [
         };
         $scope.getNumber = function(num) {
             return MoviesUtilities.getNumber(num);
+        };
+        $scope.addAmountToMovie = function (movie,amount) {
+            movie['amount'] = amount;
+        }
+        $scope.addMovieToCart = function (movie,amount) {
+            $scope.addAmountToMovie(movie,amount);
+            ShoppingDetails.movies.push.apply(ShoppingDetails.movies, movie);
         }
 
         })
@@ -242,15 +249,16 @@ var app = angular.module('myApp', [
             return MoviesUtilities.getNumber(num);
         }
     })
-.controller('ShopingCartController', function ($scope,$log,$http,$location,ShoppingDetails){
-    $scope.movies=ShoppingDetails.movies;
-    $scope.clickContinueShoping=function () {
+    .controller('ShopingCartController', function ($scope,$log,$http,$location,ShoppingDetails) {
+    $scope.movies = ShoppingDetails.movies;
+    $scope.clickContinueShoping = function () {
         $location.path('/Movies');
     }
     $scope.$on('updateShoping', function () {
-        $scope.movies= ShoppingDetails.getMovies();
+        $scope.movies = ShoppingDetails.getMovies();
 
-    });
+    })
+})
     .controller('NavController', function ($scope, UserDetails, $location) {
         $scope.userName = UserDetails.getUserName();
         $scope.$on('updateUser', function () {
@@ -273,19 +281,4 @@ var app = angular.module('myApp', [
 
     });
 
-// product class
-function movie(name, price,
-                 amount) {
-    this.name = sku; // product code (SKU = stock keeping unit)
-    this.name = name;
-    this.description = description;
-    this.price = price;
-    this.cal = cal;
-    this.nutrients = {
-        "Carotenoid": carot,
-        "Vitamin C": vitc,
-        "Folates": folate,
-        "Potassium": potassium,
-        "Fiber": fiber
-    };
-}
+
